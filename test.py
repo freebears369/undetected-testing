3d5f7f2d71732b460fba762d63b9938fd1c8aaaa
bfeng17 <63254397+bfeng17@users.noreply.github.com>
2025-10-11 12:32:27 -0400

    add

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 
-codes = ["OSALC", "UKYLC", "TYOWI", "HKGHV", "HKGXR"]
+codes = ["OSALC", "UKYLC", "TYOWI", "HKGHV", "HKGXR", 'OSAAK']
 
 with SB(uc=True, ad_block=True, test=True) as sb:
     for code in codes:
