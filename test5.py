94a5ebaacbdc116b78d7ee93426b3fc4b5d40e02
bfeng17 <63254397+bfeng17@users.noreply.github.com>
2026-04-10 22:22:54 -0400

    hm

--- test5.py
+++ test5.py
@@ -4,55 +4,36 @@ import re
 
 CODES = ["SFOKD", "SFOLC"]
 
-BASE_URL = (
-    "https://www.marriott.com/search/findHotels.mi?"
-    "fromToDate=11/30/2025&fromToDate_submit=07/02/2026"
-    "&fromDate=07/01/2026&toDate=07/02/2026"
-    "&toDateDefaultFormat=07/02/2026"
-    "&fromDateDefaultFormat=07/01/2026"
-    "&flexibleDateSearch=true&t-start=07/01/2026"
-    "&t-end=07/02/2026&lengthOfStay=1"
-    "&childrenCountBox=0+Children+Per+Room&childrenCount=0"
-    "&clusterCode=CORP&corporateCode=mma"
-    "&useRewardsPoints=false&isAdvanceSearch=false"
-    "&isNGSF=false&recordsPerPage=20"
-    "&destinationAddress.type=Hotel+Name"
-    "&destinationAddress.latitude=37.794702"
-    "&isInternalSearch=true&vsInitialRequest=false"
-    "&searchType=InCity"
-    "&destinationAddress.stateProvinceDisplayName=CA"
-    "&countryName=US&destinationAddress.stateProvince=CA"
-    "&searchRadius=50&singleSearchAutoSuggest=Unmatched"
-    "&destinationAddress.placeId=ChIJJaJK_A-BhYARkTIyu-gNgRw"
-    "&for-hotels-nearme=Near&destinationAddress.country=US"
-    "&destinationAddress.address=433+Clay+St,+San+Francisco,"
-    "+CA+94111,+USA&collapseAccordian=is-hidden"
-    "&singleSearch=true"
-    "&destinationAddress.secondaryText=Clay+Street,+San+Francisco,"
-    "+CA,+USA"
-    "&destinationAddress.city=San+Francisco"
-    "&destinationAddress.mainText=The+Jay,+Autograph+Collection"
-    "&isTransient=true"
-    "&destinationAddress.longitude=-122.4006747"
-    "&initialRequest=false"
+URL_TEMPLATE = (
+    "https://www.marriott.com/search/availabilityCalendar.mi?"
+    "propertyCode={code}&isSearch=true&currency="
+    "&lengthOfStay=1&fromDate=7/01/2026&toDate=7/02/2026"
+    "&numberOfRooms=1&guestCountBox=2+Adults+Per+Room"
+    "&childrenCountBox=0+Children+Per+Room&roomCountBox=1+Rooms"
+    "&childrenCount=0&clusterCode=corp&corporateCode=mma"
+    "&groupCode=&isHwsGroupSearch=true&flexibleDateSearch=true"
+    "&t-start=2025-11-01&t-end=2025-11-02"
+    "&fromDateDefaultFormat=11/01/2025"
+    "&toDateDefaultFormat=11/02/2025"
+    "&fromToDate_submit=11/02/2025"
+    "&fromToDate=08/23/2025"
+    "&costTab=total&isInternalSearch=true"
+    "&vsInitialRequest=false&searchType=InCity"
+    "&for-hotels-nearme=Near&collapseAccordian=is-hidden"
+    "&singleSearch=true&singleSearchAutoSuggest=Unmatched"
     "&flexibleDateSearchRateDisplay=false"
-    "&isSearch=true&isRateCalendar=true"
-    "&destinationAddress.destination=The+Jay,+Autograph+Collection,"
-    "+Clay+Street,+San+Francisco,+CA,+USA"
+    "&recordsPerPage=40&destinationAddress.latitude=35.634389"
+    "&destinationAddress.location=JW+Marriott+Hotel+Tokyo"
+    "&searchRadius=50&isTransient=true"
+    "&destinationAddress.longitude=139.73925"
+    "&initialRequest=true"
     "&isHideFlexibleDateCalendar=false"
-    "&roomCountBox=1+Room&roomCount=1"
-    "&guestCountBox=2+Adult+Per+Room&numAdultsPerRoom=2"
-    "&deviceType=desktop-web&view=list#/1/"
+    "&isFlexibleDatesOptionSelected=true"
+    "&roomCount=1&numAdultsPerRoom=2"
+    "&isAdultsOnly=false#/2/"
 )
 
-CALENDAR_URL = (
-    "https://www.marriott.com/search/availabilityCalendar.mi"
-    "?isRateCalendar=true&propertyCode={code}"
-    "&isSearch=true&currency=&costTab=total"
-    "&isAdultsOnly=false#/1/"
-)
-
-ARIA_REGEX = re.compile(
+ARIA_PATTERN = re.compile(
     r"^(?!Not available).*for.*",
     re.IGNORECASE,
 )
@@ -62,17 +43,17 @@ with SB(uc=True, ad_block=True, test=True, proxy="") as sb:
     for code in CODES:
         print(code)
 
-        sb.open(BASE_URL)
-        sb.sleep(6.5)
+        url = URL_TEMPLATE.format(code=code)
 
-        sb.open(CALENDAR_URL.format(code=code))
+        sb.open(url)
+        sb.sleep(6.5)  # let JS render
 
         html = sb.get_page_source()
         soup = BeautifulSoup(html, "html.parser")
 
         cells = soup.find_all(
             "div",
-            attrs={"aria-label": ARIA_REGEX},
+            attrs={"aria-label": ARIA_PATTERN},
         )
 
         labels = [div["aria-label"] for div in cells]
